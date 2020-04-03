f331caffcc89e7469d5e862235daf963e6cf8f4c
merlinmarijn <ratched10@hotmail.nl>
2020-04-03 12:39:50 +0200

    Add files via upload

--- State.cs
+++ State.cs
@@ -6,6 +6,4 @@ public class State : MonoBehaviour
 {
     public enum cState { Chase, Patrol, AFK };
     public cState CurrentState;
-    public enum pState {NONE,pos1, pos2, pos3, pos4 };
-    public pState CurrentPatrol;
 }
